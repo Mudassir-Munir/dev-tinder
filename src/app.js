a22321eb5dbcc6d3dd1654ea8134f9bdd9b04859
Mudassir-Munir <mudassirmunir25@gmail.com>
2026-06-08 12:32:27 +0500

    added fetch users endpoints

--- src/app.js
+++ src/app.js
@@ -16,10 +16,59 @@ app.post("/signup", async (req, res) => {
         res.send("registered successfully");
     }
     catch (err) {
-        res.Status(400).send("error while registering user: " + err.message);
+        res.status(400).send("error while registering user: " + err.message);
     }
     
 });
+
+app.get("/users", async (req, res) => {
+
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({emailId: userEmail});
+        if (users.length === 0) {
+            res.status(404).send("user not found");
+        } else {
+            res.send(users);
+        }
+    }
+    catch (err)
+    {
+        res.status(500).send("Someting went wrong: " + err.message);
+    }
+
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+
+    }
+    catch (err)
+    {
+        res.status(500).send("Someting went wrong: " + err.message);
+  
+    }
+});
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const user = await User.findOne({emailId: userEmail});
+        if (!user) {
+            res.status(404).send("user not found");
+        } else {
+            res.send(user);
+        }
+    }
+    catch (err)
+    {
+        res.status(500).send("Someting went wrong: " + err.message);
+    }
+})
 
 connectDB()
 .then(() => {
